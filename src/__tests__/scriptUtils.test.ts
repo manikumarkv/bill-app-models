c1e9791ed4183215e70c9c95efe501a3bb57f9ce
Vishal Ummadi <ummadivishal@gmail.com>
2018-11-16 17:30:34 +0530

    added more test cases

--- src/__tests__/scriptUtils.test.ts
+++ src/__tests__/scriptUtils.test.ts
@@ -19,19 +19,83 @@ test('ScriptUtilities ==> IsNullOrEmpty ==> case 4', () => {
     let myAssumptions = false
     expect(testedResult).toBe(myAssumptions);
 });
-test('ScriptUtilities ==> IsMacMachine ==> case 1', () => {
+test('ScriptUtilities ==> IsMacMachine ==> case 5', () => {
     let testedResult = ScriptUtilities.IsMacMachine();
     let myAssumptions = false
     expect(testedResult).toBe(myAssumptions);
 });
-test('ScriptUtilities ==> getNumericValue ==> case 1', () => {
+test('ScriptUtilities ==> getNumericValue ==> case 6', () => {
     let testedResult = ScriptUtilities.getNumericValue('name');
     let myAssumptions = NaN
     expect(testedResult).toBe(myAssumptions);
 });
-test('ScriptUtilities ==> getNumericValue ==> case 2', () => {
+test('ScriptUtilities ==> getNumericValue ==> case 7', () => {
     let testedResult = ScriptUtilities.getNumericValue('123');
     let myAssumptions = 123
     expect(testedResult).toBe(myAssumptions);
 });
-
+test('ScriptUtilities ==> isValidMail ==> case 8', () => {
+   let result = ScriptUtilities.validateMail('fakj@gmail.cmo')
+    let myAssumptions = true
+    expect(result).toBe(myAssumptions);
+});
+test('ScriptUtilities ==> isValidMail ==> case 9', () => {
+    let result = ScriptUtilities.validateMail('fakj@gmail')
+     let myAssumptions = false
+     expect(result).toBe(myAssumptions);
+ });
+ test('ScriptUtilities ==> isValidMail ==> case 10', () => {
+    let result = ScriptUtilities.validateMail('.cmo')
+     let myAssumptions = false
+     expect(result).toBe(myAssumptions);
+ });
+ test('ScriptUtilities ==> isValidMail ==> case 11', () => {
+    let result = ScriptUtilities.validateMail('fak.cmo')
+     let myAssumptions = false
+     expect(result).toBe(myAssumptions);
+ });
+ test('ScriptUtilities ==> isValidMail ==> case 12', () => {
+    let result = ScriptUtilities.validateMail('fakj@')
+     let myAssumptions = false
+     expect(result).toBe(myAssumptions);
+ });
+ test('ScriptUtilities ==> isValidMail ==> case 13', () => {
+    let result = ScriptUtilities.validateMail('123123@gmailo.com')
+     let myAssumptions = true
+     expect(result).toBe(myAssumptions);
+ });
+ test('ScriptUtilities ==> isValidMail ==> case 14', () => {
+    let result = ScriptUtilities.validateMail('1231_.23@gmailo.com')
+     let myAssumptions = true
+     expect(result).toBe(myAssumptions);
+ });
+ test('ScriptUtilities ==> isValidMail ==> case 15', () => {
+    let result = ScriptUtilities.validateMail('JYHGJYG@gmailo.com')
+     let myAssumptions = true
+     expect(result).toBe(myAssumptions);
+ });
+ test('ScriptUtilities ==> isValidMail ==> case 16', () => {
+    let givenResult = ScriptUtilities.validMobileNumber('827428746')
+     let myAssumptions = false
+     expect(givenResult).toBe(myAssumptions);
+ });
+ test('ScriptUtilities ==> isValidMail ==> case 17', () => {
+    let givenResult = ScriptUtilities.validMobileNumber('8274287462')
+     let myAssumptions = true
+     expect(givenResult).toBe(myAssumptions);
+ });
+ test('ScriptUtilities ==> isValidMail ==> case 18', () => {
+    let givenResult = ScriptUtilities.validMobileNumber('8274287yr')
+     let myAssumptions = false
+     expect(givenResult).toBe(myAssumptions);
+ });
+ test('ScriptUtilities ==> isValidMail ==> case 19', () => {
+    let givenResult = ScriptUtilities.validMobileNumber('wskdfshjhb')
+     let myAssumptions = false
+     expect(givenResult).toBe(myAssumptions);
+ });
+ test('ScriptUtilities ==> isValidMail ==> case 20', () => {
+    let givenResult = ScriptUtilities.validMobileNumber('82')
+     let myAssumptions = false
+     expect(givenResult).toBe(myAssumptions);
+ });
--- src/utilities/scriptUtils.ts
+++ src/utilities/scriptUtils.ts
@@ -4,7 +4,7 @@ export default class ScriptUtilities {
    * Checks object is null or undefined.
    * @param obj Object for which we need to check
    */
-  public static IsNullOrUndefined(obj: object| null| undefined): boolean {
+  public static IsNullOrUndefined(obj: object | null | undefined): boolean {
     return obj === null || obj === undefined;
   }
 
@@ -19,10 +19,23 @@ export default class ScriptUtilities {
    * Returns a  numeric value
    * @param value value of attribute
    */
-  public static getNumericValue(value: string, radix : number = 10): number| null {
+  public static getNumericValue(value: string, radix: number = 10): number | null {
     if (!StringUtils.IsNullOrEmpty(value)) {
       return parseInt(value, radix);
     }
     return null;
   }
+  public static validateMail(email: string): boolean {
+    var re = /[a-zA-Z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,3}$/;
+    return re.test(email)
+
+  }
+  public static validMobileNumber(value: string): boolean {
+    var number = /[0-9]{10}$/;
+    return number.test(value)
+  }
+  // public static HasStringAtFirst(value:string):boolean{
+
+  // }
 }
+
